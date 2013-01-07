40468e5e5adb57efad77057848958061eca23871
Andrew Arnott <andrewarnott@gmail.com>
2013-01-06 19:24:16 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9 {
+	using System.Reflection;
+
+	class Program {
+		static void Main(string[] args) {
+			var apple = new Fruit().WithColor("red").WithSkinThickness(3);
+			Console.WriteLine("You have a {0} apple with {1} skin thickness", apple.Color, apple.SkinThickness);
+		}
+	}
+}
