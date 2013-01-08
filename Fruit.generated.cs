86ea863e60eb243de9fe6651691dfecf29bacaa7
Andrew Arnott <andrewarnott@gmail.com>
2013-01-08 08:17:08 -0800

    Updates namespace

--- Fruit.generated.cs
+++ Fruit.generated.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 // ------------------------------------------------------------------------------
 
-namespace ConsoleApplication9 {
+namespace ImmutableObjectGraph {
 	using System.Diagnostics;
 	
 	public partial class Basket {
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication9 {
+namespace ImmutableObjectGraph {
 	using System.Collections.Immutable;
 	using System.Reflection;
 
