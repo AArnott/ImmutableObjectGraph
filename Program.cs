651b7987453a999ec792ea90994275f890b589d7
Kim Birkelund <github@kimbirkelund.dk>
2013-04-08 21:58:14 +0200

    Fixed using IImmutableList
    Added GetOptionalTypeName(Type) helper method.
    Added non-generic static class Optional with method For<T>(T value) :
    Optional<T> to make it easier to create Optional<T> instances.
    Changed ToImmutable() to explicitly create Optional<T> instance for each
    field.
    Changed DiscoverTemplateTypes to filter out non class types (useful for
    creating dummy types that match actual types in same interface).
    Expanded example to illustrate interface issue.
    
    This fixes the issue of the generated ToImmutable() method being invalid
    because interface typed values cannot be used as input to an implicit
    conversion. This issue is still present, and using the With() method
    with a variable typed as an interface will still not work - if it is
    typed as an implementation of that interface it will however, as
    illustrated by the various basket cases in Program.
    
    Personally I prefer this drawback to being stuck with a specific
    implementation.

--- Program.cs
+++ Program.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ImmutableObjectGraph {
-	using System.Collections.Immutable;
-	using System.Reflection;
+namespace ImmutableObjectGraph
+{
+    using System.Collections.Immutable;
+    using System.Reflection;
 
-	class Program {
-		static void Main(string[] args) {
-			var apple = Fruit.Default
-				.With(color: "red", skinThickness: 3);
-			var greenApple = apple.With(color: "green");
-			var greenAppleWithDefaultThickness = greenApple.With(skinThickness: 0);
-			var basket = Basket.Default.WithContents(ImmutableList<Fruit>.Empty.Add(apple)).WithSize(5);
-			var appleBuilder = apple.ToBuilder();
-			appleBuilder.Color = "yellow";
-			var yellowApple = appleBuilder.ToImmutable();
-			Console.WriteLine("You have a {0} apple with {1} skin thickness", apple.Color, apple.SkinThickness);
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = Fruit.Default
+                .With(color: "red", skinThickness: 3);
+            var greenApple = apple.With(color: "green");
+            var greenAppleWithDefaultThickness = greenApple.With(skinThickness: 0);
+
+            ImmutableList<Fruit> immutableFruits = ImmutableList<Fruit>.Empty.Add(apple);
+            IImmutableList<Fruit> fruits = immutableFruits;
+            var numericFruitSize = new NumericFruitSize(5);
+            IFruitSize size = numericFruitSize;
+
+            var basket = Basket.Default.With(contents: immutableFruits, size: numericFruitSize);
+            basket = Basket.Default.With(contents: Optional.For(fruits), size: Optional.For(size));
+            basket = Basket.Default.WithContents(fruits).WithSize(size);
+
+            var appleBuilder = apple.ToBuilder();
+            appleBuilder.Color = "yellow";
+            var yellowApple = appleBuilder.ToImmutable();
+            Console.WriteLine("You have a {0} apple with {1} skin thickness", apple.Color, apple.SkinThickness);
+        }
+    }
 }
