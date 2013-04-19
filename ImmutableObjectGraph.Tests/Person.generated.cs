3cf5f38876ee78cf5c8df5c772d1e5e69231a825
Andrew Arnott <andrewarnott@gmail.com>
2013-04-19 15:44:49 -0700

    Fixes test namespace.

--- ImmutableObjectGraph.Tests/Person.generated.cs
+++ ImmutableObjectGraph.Tests/Person.generated.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 // ------------------------------------------------------------------------------
 
-namespace ImmutableObjectGraph {
+namespace ImmutableObjectGraph.Tests {
 	using System.Diagnostics;
 
 	
