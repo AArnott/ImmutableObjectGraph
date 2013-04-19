67ee52eb20ea60971f8d8c415653638d4413e2bb
Andrew Arnott <andrewarnott@gmail.com>
2013-04-18 22:12:26 -0700

    Adds more Builder tests.

--- ImmutableObjectGraph.Tests/PersonBuilderTests.cs
+++ ImmutableObjectGraph.Tests/PersonBuilderTests.cs
@@ -17,5 +17,38 @@
 			Assert.Equal(person.Name, personBuilder.Name);
 			Assert.Equal(person.Age, personBuilder.Age);
 		}
+
+		[Fact]
+		public void ToBuilderToImmutableRoundtripReusesInstance() {
+			var person = Person.Default.With(name: "bill", age: 10);
+			var personBuilder = person.ToBuilder();
+			var roundTripPerson = personBuilder.ToImmutable();
+			Assert.Same(person, roundTripPerson);
+		}
+
+		[Fact]
+		public void MutablePropertiesRetainChanges() {
+			var person = Person.Default.With(name: "bill", age: 10);
+			var personBuilder = person.ToBuilder();
+
+			personBuilder.Name = "billy";
+			personBuilder.Age = 8;
+
+			Assert.Equal("billy", personBuilder.Name);
+			Assert.Equal(8, personBuilder.Age);
+		}
+
+		[Fact]
+		public void ToImmutableReturnsSimilarObject() {
+			var person = Person.Default.With(name: "bill", age: 10);
+			var personBuilder = person.ToBuilder();
+
+			personBuilder.Name = "billy";
+			personBuilder.Age = 8;
+
+			var recreatedPerson = personBuilder.ToImmutable();
+			Assert.Equal(personBuilder.Name, recreatedPerson.Name);
+			Assert.Equal(personBuilder.Age, recreatedPerson.Age);
+		}
 	}
 }
