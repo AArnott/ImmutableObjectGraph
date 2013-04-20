5b53971c229b5c9a4c6cb62c8df04830bd974051
Andrew Arnott <andrewarnott@gmail.com>
2013-04-20 09:28:18 -0700

    Adds failing unit test for new collection builder feature.

---
+++ ImmutableObjectGraph.Tests/FamilyBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ImmutableObjectGraph.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using Xunit;
+
+	public class FamilyBuilderTests {
+		[Fact]
+		public void BuildersWithCollectionsShouldUseCollectionBuilders() {
+			var billy = Person.Create(name: "billy");
+			var familyBuilder = Family.Create().ToBuilder();
+			familyBuilder.Members.Add(billy);
+			var family = familyBuilder.ToImmutable();
+			Assert.Contains(billy, family.Members);
+		}
+	}
+}
