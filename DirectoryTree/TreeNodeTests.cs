a6f24ac35a6051bc7534ab0c1c0228188278df8f
Andrew Arnott <andrewarnott@gmail.com>
2013-04-21 10:08:35 -0700

    Enhanced tree test.

--- DirectoryTree/TreeNodeTests.cs
+++ DirectoryTree/TreeNodeTests.cs
@@ -11,17 +11,21 @@
 	public class TreeNodeTests {
 		[Fact]
 		public void TreeConstruction() {
-			var root = TreeNode.Create("temp", @"c:\temp\")
-				.WithChildren(TreeNode.Create("a.cs", @"c:\temp\a.cs"));
+			var root = TreeNode.Create("temp", @"c:\temp\").WithChildren(
+				TreeNode.Create("a.cs", @"c:\temp\a.cs"),
+				TreeNode.Create("subfolder", @"c:\temp\subfolder\").WithChildren(
+					TreeNode.Create("g.h", @"c:\temp\subfolder\g.h")));
 
 			Assert.Equal("temp", root.Caption);
 			Assert.Equal("a.cs", root.Children[0].Caption);
+			Assert.Equal("subfolder", root.Children[1].Caption);
+			Assert.Equal("g.h", root.Children[1].Children[0].Caption);
 
 			var root2 = root.AddChildren(TreeNode.Create("b.cs", @"c:\temp\b.cs"));
-			Assert.Equal(2, root2.Children.Count);
+			Assert.Equal(3, root2.Children.Count);
 
 			var root3 = root2.RemoveChildren(root2.Children[0]);
-			Assert.Equal(1, root3.Children.Count);
+			Assert.Equal(2, root3.Children.Count);
 			Assert.Equal(root2.Children[1], root3.Children[0]);
 		}
 	}
