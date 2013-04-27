c7590f65c25ebb43c690e7312398cd33234650fa
Andrew Arnott <andrewarnott@gmail.com>
2013-04-27 14:44:01 -0700

    Adds tests

--- XmlNodeTree/XmlNodeTests.cs
+++ XmlNodeTree/XmlNodeTests.cs
@@ -11,12 +11,28 @@
 	public class XmlNodeTests {
 		[Fact]
 		public void XmlTreeConstruction() {
-			XmlNode root = XmlElement.Create("Root").WithChildren(
+			XmlElement root = XmlElement.Create("Root").WithChildren(
 				XmlElement.Create("Child1"),
 				XmlElement.Create("Child2"));
 
-			var xe = XmlElement.Create();
-			XmlElement result = xe.With("hi");
+			XmlElement xe = XmlElement.Create("l1", "n1");
+			XmlElement xel2 = xe.With("l2");
+			Assert.Equal("l2", xel2.LocalName);
+			Assert.Equal("n1", xel2.NamespaceName);
+			XmlElement xel3n3 = xe.With("l3", "n3");
+			Assert.Equal("l3", xel3n3.LocalName);
+			Assert.Equal("n3", xel3n3.NamespaceName);
+		}
+
+		/// <summary>
+		/// Verifies that the With overloads don't cause compiler errors due to ambiguity.
+		/// </summary>
+		[Fact]
+		public void WithNoArguments() {
+			XmlElement e = XmlElement.Create("ln1", "ns1");
+			XmlElement e2 = e.With();
+			Assert.Equal("ln1", e2.LocalName);
+			Assert.Equal("ns1", e2.NamespaceName);
 		}
 	}
 
