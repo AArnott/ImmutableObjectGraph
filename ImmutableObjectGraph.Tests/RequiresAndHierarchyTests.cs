f4f16be0a4876e95799c697561c9ad4e59ff17ce
Andrew Arnott <andrewarnott@gmail.com>
2013-04-29 19:39:58 -0700

    Adds support for [Required] fields to not be declared first.

---
+++ ImmutableObjectGraph.Tests/RequiresAndHierarchyTests.cs
@@ -0,0 +1,18 @@
+﻿namespace ImmutableObjectGraph.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using Xunit;
+
+	public class RequiresAndHierarchyTests {
+		[Fact]
+		public void RequiredFieldNotDeclaredFirst() {
+			ReqAndHierL1 value = ReqAndHierL1.Create("value2");
+			Assert.Null(value.L1Field1);
+			Assert.Equal("value2", value.L1Field2);
+		}
+	}
+}
