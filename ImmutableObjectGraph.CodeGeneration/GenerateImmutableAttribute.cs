acc96859aad529443ab77e3904ce550c07629a01
Andrew Arnott <andarno@microsoft.com>
2014-11-07 11:12:19 -0800

    Split out code generation to another assembly.

---
+++ ImmutableObjectGraph.CodeGeneration/GenerateImmutableAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace ImmutableObjectGraph.CodeGeneration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.ImmutableObjectGraph_SFG;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class GenerateImmutableAttribute : CodeGenerationAttribute
+    {
+        private string positionalArg;
+
+        public GenerateImmutableAttribute()
+        {
+        }
+
+        public GenerateImmutableAttribute(string positionalArg)
+        {
+            this.positionalArg = positionalArg;
+        }
+
+        public string NamedArg { get; set; }
+
+        public override MemberDeclarationSyntax Generate(MemberDeclarationSyntax applyTo, Document document)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)applyTo;
+
+            return SyntaxFactory.ClassDeclaration(classDeclaration.Identifier)
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword));
+        }
+    }
+}
---
+++ ImmutableObjectGraph.SFG/CodeGenerationAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.ImmutableObjectGraph_SFG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+    /// <summary>
+    /// A base attribute type for code generation attributes.
+    /// </summary>
+    public abstract class CodeGenerationAttribute : Attribute
+    {
+        /// <summary>
+        /// Create the syntax tree representing the expansion of some member to which this attribute is applied.
+        /// </summary>
+        /// <param name="applyTo">The syntax node this attribute is found on.</param>
+        /// <param name="document">The document with the semantic model in which this attribute was found.</param>
+        /// <returns>The generated member syntax to be added to the project.</returns>
+        public abstract MemberDeclarationSyntax Generate(MemberDeclarationSyntax applyTo, Document document);
+    }
+}
