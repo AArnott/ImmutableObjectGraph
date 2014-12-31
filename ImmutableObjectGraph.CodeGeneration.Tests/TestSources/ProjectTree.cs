0d7e83ee1f388969a1da3d9eac5609c935cce0c1
Andrew Arnott <andarno@microsoft.com>
2014-12-30 20:06:50 -0800

    Migrate ProjectTree but not tests yet.

---
+++ ImmutableObjectGraph.CodeGeneration.Tests/TestSources/ProjectTree.cs
@@ -0,0 +1,33 @@
+﻿namespace ImmutableObjectGraph.CodeGeneration.Tests.TestSources
+{
+    using System.Collections.Immutable;
+
+    public interface IRule { }
+
+    public interface IProjectPropertiesContext { }
+
+    public interface IPropertySheet { }
+
+    [GenerateImmutable(DefineInterface = true, GenerateBuilder = true, DefineWithMethodsPerProperty = true)]
+    partial class ProjectTree
+    {
+        [Required]
+        readonly string caption;
+        readonly string filePath;
+        readonly System.Drawing.Image icon;
+        readonly System.Drawing.Image expandedIcon;
+        readonly bool visible;
+        readonly IRule browseObjectProperties;
+        readonly ImmutableHashSet<string> capabilities;
+        readonly ImmutableSortedSet<ProjectTree> children;
+    }
+
+    [GenerateImmutable(DefineInterface = true, GenerateBuilder = true, DefineWithMethodsPerProperty = true)]
+    partial class ProjectItemTree : ProjectTree
+    {
+        [Required]
+        readonly IProjectPropertiesContext projectPropertiesContext;
+        readonly IPropertySheet propertySheet;
+        readonly bool isLinked;
+    }
+}
