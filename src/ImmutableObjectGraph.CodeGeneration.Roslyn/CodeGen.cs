972192b40f2144f2103eb5cc273b41221c68dbe1
Andrew Arnott <andrew.arnott@microsoft.com>
2015-07-12 20:50:28 -0700

    Make collection analysis properties available to all fields

--- src/ImmutableObjectGraph.CodeGeneration.Roslyn/CodeGen.cs
+++ src/ImmutableObjectGraph.CodeGeneration.Roslyn/CodeGen.cs
@@ -1059,24 +1059,9 @@
                 }
             }
 
-            public bool ChildrenAreSorted
-            {
-                get
-                {
-                    // Not very precise, but it does the job for now.
-                    return this.RecursiveParent.RecursiveField.Type.Name == nameof(ImmutableSortedSet<int>);
-                }
-            }
+            public bool ChildrenAreSorted => this.RecursiveParent.RecursiveField.IsCollectionSorted;
 
-            public bool ChildrenAreOrdered
-            {
-                get
-                {
-                    // Not very precise, but it does the job for now.
-                    var namedType = this.RecursiveParent.RecursiveField.Type as INamedTypeSymbol;
-                    return namedType != null && namedType.AllInterfaces.Any(iface => iface.Name == nameof(IReadOnlyList<int>));
-                }
-            }
+            public bool ChildrenAreOrdered => this.RecursiveParent.RecursiveField.IsCollectionOrdered;
 
             public IEnumerable<MetaField> GetFieldsBeyond(MetaType ancestor)
             {
@@ -1220,6 +1205,30 @@
             public bool IsCollection
             {
                 get { return IsCollectionType(this.Symbol.Type); }
+            }
+
+            public bool IsCollectionOrdered
+            {
+                get
+                {
+                    if (this.IsCollection)
+                    {
+                        // Not very precise, but it does the job for now.
+                        var namedType = this.Type as INamedTypeSymbol;
+                        return namedType?.AllInterfaces.Any(iface => iface.Name == nameof(IReadOnlyList<int>)) ?? false;
+                    }
+
+                    return false;
+                }
+            }
+
+            public bool IsCollectionSorted
+            {
+                get
+                {
+                    // Not very precise, but it does the job for now.
+                    return this.Type.Name == nameof(ImmutableSortedSet<int>);
+                }
             }
 
             public MetaType DeclaringType
