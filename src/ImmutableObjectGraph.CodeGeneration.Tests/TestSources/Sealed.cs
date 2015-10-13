d9880d47961d3b6ffb41d09ab2f623b0cd94d27d
Tom Glastonbury <tomg@zanyants.com>
2015-10-13 09:08:10 +0100

    Fix code formatting.

--- src/ImmutableObjectGraph.CodeGeneration.Tests/TestSources/Sealed.cs
+++ src/ImmutableObjectGraph.CodeGeneration.Tests/TestSources/Sealed.cs
@@ -1,18 +1,18 @@
 ﻿namespace ImmutableObjectGraph.CodeGeneration.Tests.TestSources
 {
-    [GenerateImmutable( DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true )]
+    [GenerateImmutable(DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true)]
     sealed partial class Sealed
     {
         readonly string name;
     }
 
-    [GenerateImmutable( DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true )]
+    [GenerateImmutable(DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true)]
     partial class Sealed_UnsealedBase
     {
         readonly string name;
     }
 
-    [GenerateImmutable( DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true )]
+    [GenerateImmutable(DefineInterface = true, DefineWithMethodsPerProperty = true, GenerateBuilder = true)]
     sealed partial class Sealed_WithBase : Sealed_UnsealedBase
     {
         readonly int age;
