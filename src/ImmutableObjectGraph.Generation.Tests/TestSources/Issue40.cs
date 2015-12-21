1f103a3c805c97ddf3fa34c72191c3ac8493a283
Andrew Arnott <andrewarnott@gmail.com>
2015-12-21 11:51:52 -0800

    Minimize repro in failing test

--- src/ImmutableObjectGraph.Generation.Tests/TestSources/Issue40.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/Issue40.cs
@@ -5,37 +5,15 @@ using ImmutableObjectGraph.Generation;
 [GenerateImmutable]
 partial class SyntaxNode
 {
-    [Required]
-    int startPosition;
-    [Required]
-    int length;
-}
-
-[GenerateImmutable]
-partial class ElseBlock : SyntaxNode
-{
-    [Required]
-    readonly string elseKeyword;
 }
 
 [GenerateImmutable]
 partial class ElseIfBlock : SyntaxNode
 {
-    [Required]
-    readonly string elseIfKeyword;
-    [Required]
-    readonly string thenKeyword;
 }
 
 [GenerateImmutable]
 partial class IfNode : SyntaxNode
 {
-    [Required]
-    readonly string ifKeyword;
-    [Required]
-    readonly string thenKeyword;
     readonly ImmutableList<ElseIfBlock> elseIfList;
-    readonly ElseBlock elseBlock;
-    [Required]
-    readonly string endKeyword;
 }
