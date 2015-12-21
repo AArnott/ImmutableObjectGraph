ccfdd946897765863cbd7443ce3b353f69863618
Andrew Arnott <andrewarnott@gmail.com>
2015-12-21 08:52:30 -0800

    Add ImmutableDictionary helper method tests
    Right now, they compile and pass because I've hard-coded what should eventually be generated code.

---
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ImmutableDictionaryHelpers.cs
@@ -0,0 +1,55 @@
+﻿namespace ImmutableObjectGraph.Generation.Tests.TestSources
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [GenerateImmutable(DefineWithMethodsPerProperty = true, DefineRootedStruct = true)]
+    partial class ImmutableDictionaryHelpers
+    {
+        readonly ImmutableDictionary<string, int> matches;
+        readonly ImmutableList<ImmutableDictionaryHelpers> children;
+
+        public ImmutableDictionaryHelpers AddMatch(string key, int value)
+        {
+            return this.With(matches: this.matches.Add(key, value));
+        }
+
+        public ImmutableDictionaryHelpers SetMatch(string key, int value)
+        {
+            return this.With(matches: this.matches.SetItem(key, value));
+        }
+
+        public ImmutableDictionaryHelpers RemoveMatch(string key)
+        {
+            return this.With(matches: this.matches.Remove(key));
+        }
+
+        static partial void CreateDefaultTemplate(ref Template template)
+        {
+            template.Matches = ImmutableDictionary.Create<string, int>();
+        }
+    }
+
+    partial struct RootedImmutableDictionaryHelpers
+    {
+        public RootedImmutableDictionaryHelpers AddMatch(string key, int value)
+        {
+            return this.With(matches: this.Matches.Add(key, value));
+        }
+
+        public RootedImmutableDictionaryHelpers SetMatch(string key, int value)
+        {
+            return this.With(matches: this.Matches.SetItem(key, value));
+        }
+
+        public RootedImmutableDictionaryHelpers RemoveMatch(string key)
+        {
+            return this.With(matches: this.Matches.Remove(key));
+        }
+
+    }
+}
