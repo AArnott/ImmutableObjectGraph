baa678f1a858c738459c44b10ac45b1c45ba5603
Elias Holzer <elias@vvvv.org>
2015-12-22 11:04:33 +0100

    Code generation fails when using an abstract class with a required field in between a type hierachy.

--- src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFields.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFields.cs
@@ -9,3 +9,17 @@ partial class Apple : Fruit
 {
     readonly string color;
 }
+
+// Type hierachy using abstract class in-between and a required field
+[ImmutableObjectGraph.Generation.GenerateImmutable]
+abstract partial class Pear : Fruit
+{
+    [ImmutableObjectGraph.Generation.Required]
+    readonly string color;
+}
+
+[ImmutableObjectGraph.Generation.GenerateImmutable]
+partial class SomePear : Pear
+{
+    readonly string someField;
+}
--- src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFieldsAndBuilder.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFieldsAndBuilder.cs
@@ -9,3 +9,17 @@ partial class Apple : Fruit
 {
     readonly string color;
 }
+
+// Type hierachy using abstract class in-between and a required field
+[ImmutableObjectGraph.Generation.GenerateImmutable(GenerateBuilder = true)]
+abstract partial class Pear : Fruit
+{
+    [ImmutableObjectGraph.Generation.Required]
+    readonly string color;
+}
+
+[ImmutableObjectGraph.Generation.GenerateImmutable(GenerateBuilder = true)]
+partial class SomePear : Pear
+{
+    readonly string someField;
+}
