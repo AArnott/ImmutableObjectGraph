e59fa99311167cd8813dfe1466d044d36c749dd9
Andrew Arnott <andrewarnott@gmail.com>
2015-12-22 06:59:31 -0800

    Move added test to its own file
    With this change, this test is compilation-based, thereby breaking the build of the test project.

--- src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFields.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFields.cs
@@ -9,17 +9,3 @@ partial class Apple : Fruit
 {
     readonly string color;
 }
-
-// Type hierachy using abstract class in-between and a required field
-[ImmutableObjectGraph.Generation.GenerateImmutable]
-abstract partial class Pear : Fruit
-{
-    [ImmutableObjectGraph.Generation.Required]
-    readonly string color;
-}
-
-[ImmutableObjectGraph.Generation.GenerateImmutable]
-partial class SomePear : Pear
-{
-    readonly string someField;
-}
--- src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFieldsAndBuilder.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ClassDerivesFromAnotherWithFieldsAndBuilder.cs
@@ -9,17 +9,3 @@ partial class Apple : Fruit
 {
     readonly string color;
 }
-
-// Type hierachy using abstract class in-between and a required field
-[ImmutableObjectGraph.Generation.GenerateImmutable(GenerateBuilder = true)]
-abstract partial class Pear : Fruit
-{
-    [ImmutableObjectGraph.Generation.Required]
-    readonly string color;
-}
-
-[ImmutableObjectGraph.Generation.GenerateImmutable(GenerateBuilder = true)]
-partial class SomePear : Pear
-{
-    readonly string someField;
-}
