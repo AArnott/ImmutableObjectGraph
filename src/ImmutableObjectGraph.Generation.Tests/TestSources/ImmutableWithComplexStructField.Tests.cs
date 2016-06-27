d3eeb73d5b73ab6687cdd6ebb97ae3eb19d45b87
Andrew Arnott <andrewarnott@gmail.com>
2016-06-26 23:03:59 -0700

    Add test for immutable with complex struct as field type
    Demonstrates bug #80

---
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ImmutableWithComplexStructField.Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace ImmutableObjectGraph.Generation.Tests.TestSources
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ImmutableWithComplexStructFieldTests
+    {
+        [Fact]
+        public void AssignVariousValues()
+        {
+            var originalStruct = new SomeStructWithMultipleFields(5, 0);
+            var structWithModifiedSecondField = new SomeStructWithMultipleFields(5, 1);
+            var v1 = ImmutableWithComplexStructField.Create(someStructField: originalStruct);
+            var v2 = v1.With(someStructField: structWithModifiedSecondField);
+            Assert.Equal(structWithModifiedSecondField.Field2, v2.SomeStructField.Field2);
+        }
+    }
+}
---
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ImmutableWithComplexStructField.cs
@@ -0,0 +1,21 @@
+﻿namespace ImmutableObjectGraph.Generation.Tests.TestSources
+{
+    [GenerateImmutable]
+    partial class ImmutableWithComplexStructField
+    {
+        readonly SomeStructWithMultipleFields someStructField;
+    }
+
+    struct SomeStructWithMultipleFields
+    {
+        internal SomeStructWithMultipleFields(int field1, int field2)
+        {
+            this.Field1 = field1;
+            this.Field2 = field2;
+        }
+
+        internal int Field1 { get; }
+
+        internal int Field2 { get; }
+    }
+}
