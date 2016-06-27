980dcae5fce2d21196eb68f0c16d3fb25ad8b3f2
Andrew Arnott <andrewarnott@gmail.com>
2016-06-27 16:36:00 -0600

    Add tests for struct with operators

--- src/ImmutableObjectGraph.Generation.Tests/TestSources/ImmutableWithComplexStructField.cs
+++ src/ImmutableObjectGraph.Generation.Tests/TestSources/ImmutableWithComplexStructField.cs
@@ -1,9 +1,13 @@
 ﻿namespace ImmutableObjectGraph.Generation.Tests.TestSources
 {
+    using System;
+
     [GenerateImmutable]
     partial class ImmutableWithComplexStructField
     {
         readonly SomeStructWithMultipleFields someStructField;
+
+        readonly SomeStructWithMultipleFieldsAndOperator someStructFieldWithOperator;
     }
 
     struct SomeStructWithMultipleFields
@@ -17,15 +21,38 @@
         internal int Field1 { get; }
 
         internal int Field2 { get; }
+    }
 
-        ////public static bool operator ==(SomeStructWithMultipleFields one, SomeStructWithMultipleFields two)
-        ////{
-        ////    return one.Field1 == two.Field1 && one.Field2 == two.Field2;
-        ////}
+    struct SomeStructWithMultipleFieldsAndOperator
+    {
+        internal SomeStructWithMultipleFieldsAndOperator(int field1, int field2)
+        {
+            this.Field1 = field1;
+            this.Field2 = field2;
+        }
 
-        ////public static bool operator !=(SomeStructWithMultipleFields one, SomeStructWithMultipleFields two)
-        ////{
-        ////    return !(one == two);
-        ////}
+        internal int Field1 { get; }
+
+        internal int Field2 { get; }
+
+        public static bool operator ==(SomeStructWithMultipleFieldsAndOperator one, SomeStructWithMultipleFieldsAndOperator two)
+        {
+            return one.Field1 == two.Field1 && one.Field2 == two.Field2;
+        }
+
+        public static bool operator !=(SomeStructWithMultipleFieldsAndOperator one, SomeStructWithMultipleFieldsAndOperator two)
+        {
+            return !(one == two);
+        }
+
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
